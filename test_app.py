f21e0ce227cd7f8822a4c9ea188d1e4cfdeae2b7
Pindosichek <mail27max@gmail.com>
2026-04-27 09:35:43 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,20 +4,22 @@ import os
 from main import init_db, add_user
 
 class TestMusicSalon(unittest.TestCase):
-    #Test1
-    def test_db_creation(self):
+    def setUp(self):
+        if os.path.exists('music_salon.db'):
+            os.remove('music_salon.db')
         init_db()
+
+    def test_db_creation(self):
         self.assertTrue(os.path.exists('music_salon.db'))
 
-    #Test2
     def test_add_user(self):
         add_user('test_admin', 'admin')
         conn = sqlite3.connect('music_salon.db')
         res = conn.execute("SELECT username FROM USERS WHERE username='test_admin'").fetchone()
+        self.assertIsNotNone(res)
         self.assertEqual(res[0], 'test_admin')
         conn.close()
 
-    #Test3
     def test_cd_table_exists(self):
         conn = sqlite3.connect('music_salon.db')
         res = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='COMPACT_DISCS'").fetchone()
