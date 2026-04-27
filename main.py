073b71ac23e077f39c46db384103248877b72d1a
Pindosichek <mail27max@gmail.com>
2026-04-27 09:39:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ def init_db():
         conn.executescript(f.read())
     conn.close()
 
-def add_user(username, role):
+def add_user(username, password_hash, role):
     conn = sqlite3.connect('music_salon.db')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO USERS (username, role) VALUES (?, ?)", (username, role))
+    cursor.execute("INSERT INTO USERS (username, password_hash, role) VALUES (?, ?, ?)", 
+                   (username, password_hash, role))
     conn.commit()
     conn.close()
